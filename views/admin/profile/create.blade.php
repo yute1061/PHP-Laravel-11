cce20d9c3338e7d6c7e975fecf8080334d555f03
Yujiro <siroidanngo1061@yahoo.co.jp>
2023-02-02 13:02:12 +0000

    課題提出のため

---
+++ views/admin/profile/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.profile')
+
+@section('title', 'プロフィールの作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
+{{-- <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, 
+        initial-scale=1">
+        
+        <title>article create</title>
+    </head>
+    <body>
+        <h1>記事の作成</h1>
+    </body>
+</html> --}}
